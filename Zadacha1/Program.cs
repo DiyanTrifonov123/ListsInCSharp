226820597ee1b1ac22415b32cf5d4f332fc286bd
STUDENTS-1212-8 <STUDENTS-1212-8@DESKTOP-PMVUDLD>
2023-03-01 11:17:04 +0200

    Add project files.

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Въведи брой елементи: ");
+            int n = int.Parse(Console.ReadLine());
+            List<int> numbers = new List<int>();
+            //input
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Въведи число: ");
+                int item = int.Parse(Console.ReadLine());
+                numbers.Add(item);
+            }
+
+            //obrabotka
+            //dobawq w sredata
+            //int newIndex = n / 2;
+            //numbers.Insert(newIndex, 6);
+
+            //dobawqne w kraya na MIN element
+            //int minElement = numbers.Min();
+            //numbers.Add(minElement);
+
+            //da iztriq 0te
+            //foreach (int chervenoKvadratche in numbers)
+            while (numbers.Contains(0))           
+            {                
+                numbers.Remove(0);
+            }
+
+            //pechat
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                Console.WriteLine($"numbers[{i}] = {numbers[i]}");
+            }
+            Console.WriteLine($"Броят на елементите е {numbers.Count}");
+        }
+    }
+}
---
+++ Zadacha2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Въведи число, до което искаш да се отпечати: ");
+            byte numFib = byte.Parse(Console.ReadLine());
+            List<ulong> numbers = new List<ulong>();
+            numbers.Add(1);
+            numbers.Add(1);
+
+            for (int i = 2; i < numFib; i++)
+            {
+                numbers.Add(numbers[i - 2] + numbers[i - 1]);
+            }
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
