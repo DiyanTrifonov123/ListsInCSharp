f905ca574bee9fefa7921c921dd8e9862e12c664
DiyanTrifonov123 <diyantrifonov5@gmail.com>
2023-03-02 10:32:10 +0200

    Zadacha2.3.2023

---
+++ Zadacha3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Въведи числа:");
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            byte count = 1;
+            byte maxCount = 0;
+            int repeatedElement = 0;
+
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                if (numbers[i] == numbers[i + 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    if (count > maxCount)
+                    {
+                        repeatedElement = numbers[i];
+                        maxCount = count;
+                    }
+                    count = 1;
+                }
+            }
+            Console.WriteLine($"{repeatedElement} se povtarq {maxCount} puti!");
+        }
+    }
+}
---
+++ Zadacha4/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Въведи числа на списъка: ");
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            PrintCount(numbers);
+
+            Console.Write("Въведи числа на първия масив: ");
+            int[] array1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            AddArrayToList(numbers, array1);
+
+            Console.Write("Въведи число, за да провериш дали го има: ");
+            int num = int.Parse(Console.ReadLine());
+            IsNumInList(numbers, num);
+
+            Console.Write("Въведи числа на втория масив: ");
+            int[] array2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            AddArrayToListTwo(numbers, array2);
+
+            Console.WriteLine();
+        }
+
+        private static void AddArrayToListTwo(List<int> numbers, int[] array2)
+        {
+            numbers.InsertRange(3, array2);
+            Console.WriteLine(string.Join(", ",numbers));
+            numbers.Sort();
+            Console.Write($"Добавяме втория масив към списъка с първия масив {string.Join("*", numbers)}");
+        }
+
+        private static void IsNumInList(List<int> numbers, int num)
+        {
+            if (numbers.Contains(num))
+            {
+                Console.WriteLine($"Числото {num} е на {numbers.IndexOf(num)}!");
+            }
+            else
+            {
+                Console.WriteLine($"Числото {num} не е в списъка!");
+            }
+        }
+
+        private static void AddArrayToList(List<int> numbers, int[] array1)
+        {
+            numbers.AddRange(array1);
+            numbers.Sort();
+            Console.WriteLine($"Добавя се първия масива към списъка: {string.Join(" ", numbers)}");
+        }
+
+        private static void PrintCount(List<int> numbers)
+        {
+            Console.WriteLine($"Броя на елементите от списъка: {numbers.Count}");
+        }
+    }
+}
